f3c940fff669d61c27a14ea5c70b838b02544268
amrElzohairy <aelzohairy9@gmail.com>
2025-04-13 17:50:28 +0200

    add auth repo logic

--- lib/feature/auth/domain/repo/auth_repo.dart
+++ lib/feature/auth/domain/repo/auth_repo.dart
@@ -1,3 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:rika_online_store/core/errors/failure.dart';
+import 'package:rika_online_store/feature/auth/domain/entities/user_entity.dart';
+
 abstract class AuthRepo {
-  
+  Future<Either<Failure,UserEntity>> signUp();
+  Future<Either<Failure,UserEntity>> signIn();
 }
