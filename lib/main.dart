a747680d16534f2649f40f175c76d25b2829e300
amrElzohairy <aelzohairy9@gmail.com>
2025-04-13 14:05:03 +0200

    fixs debugging badge

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class RikaStore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Rika Store',
       theme: ThemeData(),
       home: const Scaffold(),
