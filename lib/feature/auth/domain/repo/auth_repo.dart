ee28c3f9c96cb21c4be55eb5a69507889963fb0f
amrElzohairy <aelzohairy9@gmail.com>
2025-04-13 18:09:17 +0200

    fix in repo

--- lib/feature/auth/domain/repo/auth_repo.dart
+++ lib/feature/auth/domain/repo/auth_repo.dart
@@ -4,6 +4,6 @@ import 'package:rika_online_store/feature/auth/data/models/sign_up_body.dart';
 import 'package:rika_online_store/feature/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<Either<Failure, UserEntity>> signUp(SignUpBody signUpBody);
+  Future<Either<Failure, String>> signUp(SignUpBody signUpBody);
   Future<Either<Failure, UserEntity>> signIn();
 }
