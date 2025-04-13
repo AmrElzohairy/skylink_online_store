063f65c719e76022e812672530c2f34301b16f15
amrElzohairy <aelzohairy9@gmail.com>
2025-04-13 15:14:20 +0200

    add text styles

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -2,5 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const lightPrimary = Color(0XFF000000);
-  static const lightgrey = Color(0XFF666666);
+  static const black = Color(0XFF000000);
+  static const white = Color(0XFFFFFFFF);
+  static const textGray1 = Color(0XFF666666);
+  static const textGray2 =  Color(0xFFAAAAAA);
 }
---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:rika_online_store/core/utils/app_colors.dart';
+
+class AppStyles {
+  static final TextStyle semiBold20Black = const TextStyle(
+    color: AppColors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static final TextStyle semiBold14Gray1 = const TextStyle(
+    color: AppColors.textGray1,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+  static final TextStyle semiBold14Gray2 = const TextStyle(
+    color: AppColors.textGray2,
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+  static final TextStyle semiBold20Gray2 = const TextStyle(
+    color: AppColors.textGray2,
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static final TextStyle extraBold16White = const TextStyle(
+    color: AppColors.white,
+    fontSize: 16,
+    fontWeight: FontWeight.w800,
+  );
+  static final TextStyle extraBold18Black = const TextStyle(
+    color: AppColors.black,
+    fontSize: 18,
+    fontWeight: FontWeight.w800,
+  );
+  static final TextStyle extraBold20Black = const TextStyle(
+    color: AppColors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w800,
+  );
+  static final TextStyle extraBold25Black = const TextStyle(
+    color: AppColors.black,
+    fontSize: 25,
+    fontWeight: FontWeight.w800,
+  );
+}
