778cccedd7fbd527fe2dbbd7c1d46163c4abcdfa
amrElzohairy <aelzohairy9@gmail.com>
2025-04-14 14:57:11 +0200

    add logic to sign in methode

--- lib/feature/auth/data/repo/auth_repo_impl.dart
+++ lib/feature/auth/data/repo/auth_repo_impl.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:rika_online_store/core/errors/failure.dart';
 import 'package:rika_online_store/core/networking/api_end_points.dart';
 import 'package:rika_online_store/core/networking/dio_client.dart';
+import 'package:rika_online_store/feature/auth/data/models/sign_in_body.dart';
 import 'package:rika_online_store/feature/auth/data/models/sign_up_body.dart';
 import 'package:rika_online_store/feature/auth/domain/entities/user_entity.dart';
 import 'package:rika_online_store/feature/auth/domain/repo/auth_repo.dart';
@@ -14,9 +15,22 @@ class AuthRepoImpl extends AuthRepo {
 
   AuthRepoImpl({required this.dio});
   @override
-  Future<Either<Failure, UserEntity>> signIn() {
-    // TODO: implement signIn
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity>> signIn(SignInBody signInBody) async {
+    try {
+      var resonse = await dio.post(
+        ApiEndpoints.login,
+        data: signInBody.toJson(),
+      );
+      var userData = UserEntity.fromUser(resonse.data);
+      return Right(userData);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        log("Dio error in signIn method in AuthRepoImpl: $e");
+        return Left(ServerFailure.fromDiorError(e));
+      }
+      log("Error in signIn method in Catch in AuthRepoImpl: $e");
+      return Left(ServerFailure(e.toString()));
+    }
   }
 
   @override
@@ -29,7 +43,7 @@ class AuthRepoImpl extends AuthRepo {
         log("Dio error in signUp method in AuthRepoImpl: $e");
         return Left(ServerFailure.fromDiorError(e));
       }
-      log("Error in signUp method in AuthRepoImpl: $e");
+      log("Error in signUp method in Catch in AuthRepoImpl: $e");
       return Left(ServerFailure(e.toString()));
     }
   }
--- lib/feature/auth/domain/repo/auth_repo.dart
+++ lib/feature/auth/domain/repo/auth_repo.dart
@@ -1,9 +1,10 @@
 import 'package:dartz/dartz.dart';
 import 'package:rika_online_store/core/errors/failure.dart';
+import 'package:rika_online_store/feature/auth/data/models/sign_in_body.dart';
 import 'package:rika_online_store/feature/auth/data/models/sign_up_body.dart';
 import 'package:rika_online_store/feature/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
   Future<Either<Failure, void>> signUp(SignUpBody signUpBody);
-  Future<Either<Failure, UserEntity>> signIn();
+  Future<Either<Failure, UserEntity>> signIn(SignInBody signInBody);
 }
